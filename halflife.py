c5aa4cce0b4b38f54bfd80812faced60a67082c4
Potatochips2001 <76017734+Potatochips2001@users.noreply.github.com>
2020-12-23 06:18:57 -0500

    halflife.py
    Added live absorption counter

--- halflife.py
+++ halflife.py
@@ -1,7 +1,8 @@
 import time
 import math
 
-def halflife(dose, half_life, timesincedose):
+#dose, half-life, time since tmax
+def ke(dose, half_life, timesincedose):
     ke = math.log(2)/half_life
     remaining = dose * math.pow(math.e, -ke*timesincedose)
     return remaining
@@ -9,7 +10,7 @@ def halflife(dose, half_life, timesincedose):
 
 # dose, half-life in seconds. Dosage remaining will update every one(1) second
 # Ctrl+c to stop
-def live(dose, half_life):
+def liveke(dose, half_life):
     try:
         time_ = 0
         ke = math.log(2)/half_life
@@ -24,3 +25,27 @@ def live(dose, half_life):
                 return "Stopped live elimination counter"
     except Exception as e:
         return e
+
+#dose, half-life in seconds. Dosage absorbed will update every one(1) second
+# Ctrl+c to stop
+def liveka(dose, half_life):
+    try:
+        time_ = 0
+        ka = math.log(2)/half_life
+        absorbed = dose - (dose * math.pow(math.e, -ka*0))
+        while True:
+            try:
+                absorbed = dose - (dose * math.pow(math.e, -ka*time_))
+                print("Dose absorbed: " + str(absorbed), end='\r')
+                time.sleep(1)
+                time_ += 1
+            except KeyboardInterrupt:
+                return "Stopped live absorption counter"
+    except Exception as e:
+        return e
+
+#dose, half-life, time since dosing
+def ka(dose, half_life, timesincedose):
+    ka = math.log(2)/half_life
+    absorbed = dose - (dose*math.pow(math.e, -ka*timesincedose))
+    return absorbed
